301da534d7a8d2fb070ef6d91efb864175d99b7c
HediehKh <113653779+HediehKh@users.noreply.github.com>
2023-05-16 20:40:54 -0700

    tutorial

---
+++ components/BackButton/index.js
@@ -0,0 +1,11 @@
+import styles from 'components/BackButton/BackButton.module.css'
+export default function BackButton() {
+    return(
+        <>
+        <div >
+        <button className={styles.button}>Go Back</button>
+        </div>
+        
+        </>
+    )
+}
