a61d90a9b3b33ba3fc856486c473617478f10baa
Ivona <ivona.i.work@gmail.com>
2023-05-16 22:15:38 -0700

    new files

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,52 @@
 import NavBar from "@/components/NavBar"
+import Head from "next/head"
+import Image from "next/image"
+import styles from 'styles/Profile.module.css'
 export default function Profile() {
     return(
         <>
-        <NavBar/>
+          <main>               
+              
+              <div className={styles.profileContainer}>                  
+                <div className={styles.profileInfo}>                     
+                   <Image className={styles.profileImage}                      
+                        src='/images/UserImage.png'                           
+                                                  
+                              width={200}                          
+                                 height={200} />
+                   <h1>Sammy Ham</h1>    
+                    <div>                         
+                      <div>                           
+                          <div>                              
+                                   <div className={styles.personalInfo}>                                   
+                                          <p>BIRTHDAY</p>                         
+                                                  <p>03/20/1999</p>                   
+                                                      </div>                   
+                                                        <div className={styles.personalInfo}>          
+                                                                                                                          
+                                                             <p>EMAIL</p>                               
+                                                                                                        
+                                                                   <p>SammyHam@email.com</p>                
+                                    
+                                                    </div>                     
+                                    </div>               
+                           <div>                
+            <div className={styles.personalInfo}>                                  
+              <p>PHONE</p>                                    
+                  <p>(123)456-7777</p>                            
+                        </div>                           
+                         </div>                                
+                                    </div>                                
+                                       </div>              
+                                       
+                                         </div>   
+                                         </div>
+                                         <NavBar/>    
+                                         
+                     </main>                     
+       
+        
 
         </>
-    )
+)
 }
